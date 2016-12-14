c0f2673b9388becc4af8db04de7e34d6a359ea6b
baileymader <bailey@baileymader.com>
2016-12-13 21:24:41 -0600

    Coin Return implemented

--- TestVendingMachine/UnitTest1.cs
+++ TestVendingMachine/UnitTest1.cs
@@ -18,43 +18,54 @@ namespace TestVendingMachine
         [TestMethod]
         public void TestInsertNickel()
         {
-            var preCount = testMachine.coinCount(Coins.NICKELS);
+            var preCount = testMachine.CoinCount(Coins.NICKEL);
             var preAmount = testMachine.AmountDeposited;
             testMachine.InsertCoin(5);
-            Assert.AreEqual(preCount, testMachine.coinCount(Coins.NICKELS) - 1);
+            Assert.AreEqual(preCount, testMachine.CoinCount(Coins.NICKEL) - 1);
             Assert.AreEqual(preAmount, testMachine.AmountDeposited - 5);
         }
 
         [TestMethod]
         public void TestInsertDime()
         {
-            var preCount = testMachine.coinCount(Coins.DIMES);
+            var preCount = testMachine.CoinCount(Coins.DIME);
             var preAmount = testMachine.AmountDeposited;
             testMachine.InsertCoin(10);
-            Assert.AreEqual(preCount, testMachine.coinCount(Coins.DIMES) - 1);
+            Assert.AreEqual(preCount, testMachine.CoinCount(Coins.DIME) - 1);
             Assert.AreEqual(preAmount, testMachine.AmountDeposited - 10);
         }
 
         [TestMethod]
         public void TestInsertQuarter()
         {
-            var preCount = testMachine.coinCount(Coins.QUARTERS);
+            var preCount = testMachine.CoinCount(Coins.QUARTER);
             var preAmount = testMachine.AmountDeposited;
             testMachine.InsertCoin(25);
-            Assert.AreEqual(preCount, testMachine.coinCount(Coins.QUARTERS) - 1);
+            Assert.AreEqual(preCount, testMachine.CoinCount(Coins.QUARTER) - 1);
             Assert.AreEqual(preAmount, testMachine.AmountDeposited - 25);
         }
 
         [TestMethod]
         public void TestInsertPenny()
         {
-            var preCount = testMachine.coinReturnCount(Coins.INVALID);
+            var preCount = testMachine.CoinReturnCount(Coins.INVALID);
             var preAmount = testMachine.AmountDeposited;
             testMachine.InsertCoin(1);
-            Assert.AreEqual(preCount, testMachine.coinReturnCount(Coins.INVALID) - 1);
+            Assert.AreEqual(preCount, testMachine.CoinReturnCount(Coins.INVALID) - 1);
             Assert.AreEqual(preAmount, testMachine.AmountDeposited);
         }
 
+        [TestMethod]
+        public void TestReturnCoins()
+        {
+            testMachine.InsertCoin(5);
+            testMachine.InsertCoin(10);
+            testMachine.InsertCoin(25);
+            testMachine.ReturnCoins();
+            Assert.AreEqual(1, testMachine.CoinReturnCount(Coins.NICKEL));
+            Assert.AreEqual(1, testMachine.CoinReturnCount(Coins.DIME));
+            Assert.AreEqual(1, testMachine.CoinReturnCount(Coins.QUARTER));
+        }
 
     }
 }
--- VendingMachineKata/VendingMachine.cs
+++ VendingMachineKata/VendingMachine.cs
@@ -8,35 +8,52 @@ namespace VendingMachineKata
 {
     public enum Coins
     {
-        NICKELS,
-        DIMES,
-        QUARTERS,
+        NICKEL,
+        DIME,
+        QUARTER,
         INVALID
     }
+    public enum Products
+    {
+        POP,
+        CHIPS,
+        CANDY
+    }
+
     public class VendingMachine
     {
+        public const string INSERT_COIN = "INSERT COIN";
+        public const string THANK_YOU = "THANK YOU";
+        public const string SOLD_OUT = "SOLD OUT";
+        public const string PRICE = "PRICE";
 
-        private int pop;
-        private int chips;
-        private int candy;
-        private int[] coinsInKitty = { 10, 10, 10 };
+        public const float POP_PRICE = 1.00F;
+        public const float CHIP_PRICE = 0.50F;
+        public const float CANDY_PRICE = 0.65F;
+
+        private int[] productsAvailable = { 10, 10, 10 };
+        private int[] coinsAvailable = { 10, 10, 10 };
         private int[] coinsDeposited = { 0, 0, 0 };
         private int[] coinReturn = { 0, 0, 0, 0 };
         private float amountDeposited = 0;
+        private string displayString;
 
         public float AmountDeposited
         {
             get { return amountDeposited; }
         }
 
-        public VendingMachine()
+        public string DisplayString
         {
-            pop = 10;
-            chips = 10;
-            candy = 10;
+            get { return displayString; }
         }
 
-        public int coinCount(Coins coinType)
+        public VendingMachine()
+        {
+            displayString = "INSERT COIN";
+        }
+
+        public int CoinCount(Coins coinType)
         {
             return coinsDeposited[(int)coinType];
         }
@@ -46,27 +63,46 @@ namespace VendingMachineKata
             switch (coinValue)
             {
                 case 5:
-                    coinsDeposited[(int)Coins.NICKELS]++;
+                    coinsDeposited[(int)Coins.NICKEL]++;
                     amountDeposited += 5;
                     break;
                 case 10:
-                    coinsDeposited[(int)Coins.DIMES]++;
+                    coinsDeposited[(int)Coins.DIME]++;
                     amountDeposited += 10;
                     break;
                 case 25:
-                    coinsDeposited[(int)Coins.QUARTERS]++;
+                    coinsDeposited[(int)Coins.QUARTER]++;
                     amountDeposited += 25;
                     break;
                 default:
                     coinReturn[(int)Coins.INVALID]++;
                     break;
             }
-
+            UpdateDisplay();
         }
 
-        public int coinReturnCount(Coins coinType)
+        private void UpdateDisplay()
+        {
+            if (amountDeposited > 0)
+            {
+                displayString = "$" + amountDeposited.ToString();
+            }
+            else
+            {
+                displayString = "INSERT COIN";
+            }
+        }
+
+        public int CoinReturnCount(Coins coinType)
         {
             return coinReturn[(int)coinType];
+        }
+
+        public void ReturnCoins()
+        {
+            coinReturn[(int)Coins.NICKEL] += coinsDeposited[(int)Coins.NICKEL];
+            coinReturn[(int)Coins.DIME] += coinsDeposited[(int)Coins.DIME];
+            coinReturn[(int)Coins.QUARTER] += coinsDeposited[(int)Coins.QUARTER];
         }
     }
 }
